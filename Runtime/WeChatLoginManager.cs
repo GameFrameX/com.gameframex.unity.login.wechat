3734340bf1721e70ae16a862648920b3c81b60c0
Blank <wangfj11@foxmail.com>
2025-10-28 11:32:18 +0800

    feat(微信登录): 在编辑器环境下模拟微信登录成功
    在Unity编辑器环境下直接模拟微信登录成功回调，方便开发测试

--- Runtime/WeChatLoginManager.cs
+++ Runtime/WeChatLoginManager.cs
@@ -91,6 +91,10 @@ namespace GameFrameX.Login.WeChat.Runtime
         {
             _loginSuccess = loginSuccess;
             _loginFail = loginFail;
+#if UNITY_EDITOR
+            _loginSuccess?.Invoke(new WeChatLoginSuccess() { NickName = "test", OpenId = SystemInfo.deviceUniqueIdentifier, PhotoUrl = "test", UnionId = SystemInfo.deviceUniqueIdentifier });
+            return;
+#endif
             _shareSDK.Authorize(PlatformType.WeChat);
         }
 
